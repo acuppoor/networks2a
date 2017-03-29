2623a40e3d3d6400301df8c26179cc7b8d32d3e3
Moondoll5 <moondoll101@gmail.com>
2017-03-29 15:04:22 +0200

    Update

--- Server.java
+++ Server.java
@@ -1,1 +1,28 @@
 //aaa
+public class Server {
+   ServerSocket serverPort;
+    Socket clientPort;
+    
+    public static void main(String[] args){
+        //Get how many people in chat
+        Scanner s = new Scanner(System.in);
+        System.out.print("How many people are joining the chat? ");
+        int people = Integer.parseInt(s.next());
+            
+    }
+    
+    public void createServerSockets(){
+        try {
+            ServerSocket ss = new ServerSocket();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+    
+    public void checkForConnection(){
+        while (true) {            
+            clientPort = ss.accept();
+        }
+    }
+    
+}
