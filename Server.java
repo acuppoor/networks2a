95eb43ac40c502a3c89e21f905d00fdf2cbfd4ef
Arjun K.S.S. Cuppoor <acuppoor@yahoo.com>
2017-03-30 17:48:56 +0200

    Add files via upload

--- Server.java
+++ Server.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.net.*;
 
 
 public class Server {
@@ -13,27 +14,55 @@ public class Server {
     static ServerSocket serverSock;
     static Socket clientSock;
     static DataInputStream input;
-    static PrintStream output;
+    static DataOutputStream output;
+   
     
     public static void main(String[] args) throws IOException{ //Exceptions thrown for method
-        //Get how many people in chat
-        Scanner s = new Scanner(System.in);
-        System.out.print("How many people are joining the chat? ");
-        int people = Integer.parseInt(s.next());
-        
-        //Creates a server socket which listens for connections from clients.
-        serverSock = new ServerSocket(portNo); //DOES THIS HAVE TO BE THE SAME PORT NO AS THE CLIENTS?
-        
-        //Server waits for connection request from a client
-        //Accepts the connection from the client
-        //Assigns a unique port to the client
-        clientSock = serverSock.accept(); 
-        
-        //Gets input from client to send through server.
-        input = new DataInputStream(clientSock.getInputStream());
-        
-        //Sends output to a client
-        output = new PrintStream(clientSock.getOutputStream());
+		String messageFromClient;
+		String messageToClient;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many people will chat? \n");
+		int numberOfPeople = 2;
+		
+		try{
+			numberOfPeople = scanner.nextInt();
+		} catch (Exception e){
+			numberOfPeople = 2;
+			System.out.println("Number of people is assumed to be 2, error with the number of people entered:" + e.getStackTrace());
+		}
+		
+		ServerSocket[] serverSockets = new ServerSocket[numberOfPeople];
+		String[] portNumbers = new String[numberOfPeople];
+		Socket[] sockets = new Socket[numberOfPeople];
+		
+		System.out.println("Enter the " + numberOfPeople + " port numbers (separated by space):" );
+		
+		for (int i = 0; i < numberOfPeople; i++){
+			portNumbers[i] = scanner.next();
+			serverSockets[i] = new ServerSocket(Integer.parseInt(portNumbers[i]));
+		}
+		
+		ClientHandler[] clientHandlers = new ClientHandler[numberOfPeople];
+		
+		/*ServerSocket welcomeSocket = new ServerSocket(2222);	
+			
+		while(true)
+		{	
+			Socket connectionSocket = welcomeSocket.accept();
+			ClientHandler clientHandler = new ClientHandler(connectionSocket);
+			clientHandler.start();
+		}*/
+		
+		while(true){
+			for(int i = 0; i < numberOfPeople; i++){
+				sockets[i] = serverSockets[i].accept();
+				DataInputStream fromClient = new DataInputStream(sockets[i].getInputStream());
+				String clientMessage = fromClient.readLine();
+				
+				clientHandlers[i] = new ClientHandler(clientMessage, sockets, i);
+				clientHandlers[i].start();				
+			}
+		}
     }  
     
 }
